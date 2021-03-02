6552430f3c72999360ef6722955c85c78534c418
Hosein-Ahmadzade <64315919+Hosein-Ahmadzade@users.noreply.github.com>
2021-03-02 14:56:52 +0330

    The definition of some errors in Python added to our Python file.

---
+++ error_handling.py
@@ -0,0 +1,27 @@
+# Error Handling
+
+"""
+
+An error that crashes our program is called an exception and Python raises these exceptions whenever the interpreter says hey I have no idea what are you donig and something is wrong and then I'm going to stop the program and give you an output (those red outputs that means errors). Well this is bad. For example if we are google and people depend on us to maybe check their emails or to watch YouTube videos, so if our programs have exceptions and they stop working and they error out, that's bad and that could cost companies millions. So how can we handle these expections that crash our program? Well, we need something error handling.
+
+Error Handling:
+
+So instead of letting our programs just error, we're able to handle them so that if we get something wrong like a TypeError, we can actually handle that error within our programs.
+
+Error Handling allows us to let Python script continue running even if there are errors.
+
+Some of the errors that exist in Python:
+
+TypeError: When we trying do something between two different data types that aren't compatible.
+Syntax Error: When we  write a code that is not a standard python syntax.
+NameError: When our name of variable is not defined.
+IndexError: When we have a list that has 3 items(0 to 2) and we want to access index of 3. 
+KeyError: When we have a dictionary and we trying to access something that doesn't exist.
+ZeroDivisionError: When we divide a number by 0 like this. 5/0.
+
+...
+
+There are alot of these errors in Python and I'll link to this resource.
+
+
+"""
